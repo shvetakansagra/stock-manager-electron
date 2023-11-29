ca5c45f34f2779c0262a30bede7c4082acdb71b0
shveta <112923827+shvetakansagra@users.noreply.github.com>
2023-11-29 09:51:00 +0530

    update

--- src/database.js
+++ src/database.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2/promise');
 
+// const connection =mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     database:'stock_managment'
+// })
 const connection =mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'stock_managment'
+    host:'154.56.47.154',
+    user:'u344208509_invoice',
+    password:'9EbfSGW~LgMM',
+    database:'u344208509_invoice'
 })
 
 function getConnection(){
